ea30d0840fea6ec364954cb5b594e574a1b69496
captain-pirate <1933270533@qq.com>
2019-12-05 10:57:57 +0800

    first commit

---
+++ src/main/java/com/qy105/aaa/Test.java
@@ -0,0 +1,16 @@
+package com.qy105.aaa;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author AI
+ * @title: Test
+ * @projectName 1205-test
+ * @description: TODO
+ * @date 2019/12/510:02
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
